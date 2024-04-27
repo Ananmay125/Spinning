e6b4e338e5d0a919d7f19de2035260f11a1095a1
Ananmay 125 <ananmay125@gmail.com>
2024-04-27 12:31:19 +0530

    audio, background, title

--- core/src/com/mygdx/spinning/spin.java
+++ core/src/com/mygdx/spinning/spin.java
@@ -9,6 +9,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
@@ -17,30 +18,41 @@ public class spin extends ApplicationAdapter {
     private SpriteBatch spriteBatch;
     private Viewport viewport;
     private Sprite sprite;
+    private Sprite centerSprite;
     private float originalY;
     private float velocityY = 0;
     private boolean jumping = false;
+    private boolean facingRight = true;
+
+    private Music backgroundMusic;
 
     @Override
     public void create() {
         Gdx.graphics.setFullscreenMode(Gdx.graphics.getDisplayMode());
         viewport = new ExtendViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         shapeRenderer = new ShapeRenderer();
-
         spriteBatch = new SpriteBatch();
 
-        Texture texture = new Texture("assets/mario.png");
+        backgroundMusic = Gdx.audio.newMusic(Gdx.files.internal("assets/sansd.mp3"));
+        backgroundMusic.setLooping(true);
+        backgroundMusic.play();
 
+        Texture texture = new Texture("assets/mario.png");
         sprite = new Sprite(texture);
         sprite.setPosition(100, 100);
         sprite.setSize(100,100);
+
+        Texture centerTexture = new Texture("assets/title.png");
+        centerSprite = new Sprite(centerTexture);
+        centerSprite.setScale(0.5F,0.5F);
+        centerSprite.setPosition((Gdx.graphics.getWidth() - centerSprite.getWidth()) / 2, (Gdx.graphics.getHeight() - centerSprite.getHeight()) / 2);
 
         originalY = sprite.getY();
     }
 
     @Override
     public void render() {
-        Gdx.gl.glClearColor(0.5f, 0.7f, 1.0f, 1.0f);
+        Gdx.gl.glClearColor(0.0f,0.0f,1.0f,0.0f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         viewport.apply();
@@ -51,7 +63,7 @@ public class spin extends ApplicationAdapter {
         shapeRenderer.rect(0, 0, viewport.getWorldWidth(), 100);
         shapeRenderer.end();
 
-        float gravity = -300;
+        float gravity = -600;
         velocityY += gravity * Gdx.graphics.getDeltaTime();
         sprite.translateY(velocityY * Gdx.graphics.getDeltaTime());
 
@@ -65,17 +77,34 @@ public class spin extends ApplicationAdapter {
 
         spriteBatch.setProjectionMatrix(viewport.getCamera().combined);
         spriteBatch.begin();
+        if (!facingRight) {
+            sprite.setFlip(true, false);
+        } else {
+            sprite.setFlip(false, false);
+        }
         sprite.draw(spriteBatch);
+        centerSprite.draw(spriteBatch);
         spriteBatch.end();
     }
 
     private void handleInput() {
-        float movementSpeed = 200;
+        float movementSpeed = 600;
         if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
             sprite.setX(sprite.getX() - movementSpeed * Gdx.graphics.getDeltaTime());
+            facingRight = false;
         }
         if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
             sprite.setX(sprite.getX() + movementSpeed * Gdx.graphics.getDeltaTime());
+            facingRight = true;
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+            sprite.setX(sprite.getX() - movementSpeed * Gdx.graphics.getDeltaTime());
+            facingRight = false;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.D)) {
+            sprite.setX(sprite.getX() + movementSpeed * Gdx.graphics.getDeltaTime());
+            facingRight = true;
         }
 
         if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE) && !jumping) {
@@ -93,5 +122,6 @@ public class spin extends ApplicationAdapter {
     public void dispose() {
         shapeRenderer.dispose();
         spriteBatch.dispose();
+        backgroundMusic.dispose();
     }
 }
